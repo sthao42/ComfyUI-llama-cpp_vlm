5251c5abd9e09f169c09b7567ef622bb83c0c4be
sthao42 <sirgrimz@proton.me>
2026-08-08 02:34:55 -0500

    fix dynamic socket expansion and collapse logic in dynamic_image_inputs.js using graph link verification

--- web/js/dynamic_image_inputs.js
+++ web/js/dynamic_image_inputs.js
@@ -9,15 +9,23 @@ app.registerExtension({
                 const me = onNodeCreated?.apply(this, arguments);
                 const MAX_IMAGES_INDEX = 7; // Total 8 sockets: image_0 to image_7
                 
-                const updateImageInputs = () => {
+                const updateImageInputs = (changedSlot, isConnectedEvent) => {
                     if (!this.inputs || this._configuring) return;
                     
                     let maxConnectedNum = -1;
-                    for (const input of this.inputs) {
+                    for (let idx = 0; idx < this.inputs.length; idx++) {
+                        const input = this.inputs[idx];
                         if (input && input.name && input.name.startsWith("image_")) {
                             const num = parseInt(input.name.replace("image_", ""), 10);
                             if (!isNaN(num)) {
-                                const isConnected = input.link != null && input.link !== undefined;
+                                let isConnected = false;
+                                if (idx === changedSlot && isConnectedEvent !== undefined) {
+                                    isConnected = isConnectedEvent;
+                                } else if (input.link != null) {
+                                    const linkObj = this.graph ? (this.graph.links ? this.graph.links[input.link] : null) : null;
+                                    isConnected = linkObj != null;
+                                }
+                                
                                 if (isConnected && num > maxConnectedNum) {
                                     maxConnectedNum = num;
                                 }
@@ -32,11 +40,9 @@ app.registerExtension({
                         const inputName = `image_${i}`;
                         const existing = this.inputs.find(inp => inp && inp.name === inputName);
                         if (!existing) {
-                            // Find queue_handler index to insert image_X above queue_handler
                             const queueIdx = this.inputs.findIndex(inp => inp && inp.name === "queue_handler");
                             if (queueIdx !== -1) {
                                 this.addInput(inputName, "IMAGE");
-                                // Move the newly added input above queue_handler
                                 const newInput = this.inputs.pop();
                                 this.inputs.splice(queueIdx, 0, newInput);
                             } else {
@@ -50,8 +56,11 @@ app.registerExtension({
                         const input = this.inputs[i];
                         if (input && input.name && input.name.startsWith("image_")) {
                             const num = parseInt(input.name.replace("image_", ""), 10);
-                            if (!isNaN(num) && num > targetMax && (input.link == null || input.link === undefined)) {
-                                this.removeInput(i);
+                            if (!isNaN(num) && num > targetMax) {
+                                const isConnected = input.link != null && (this.graph?.links ? this.graph.links[input.link] != null : true);
+                                if (!isConnected) {
+                                    this.removeInput(i);
+                                }
                             }
                         }
                     }
@@ -64,23 +73,10 @@ app.registerExtension({
                     if (origOnConnectionsChange) {
                         origOnConnectionsChange.apply(this, arguments);
                     }
-                    setTimeout(() => updateImageInputs(), 20);
-                };
-
-                const origOnConnectInput = this.onConnectInput;
-                this.onConnectInput = function () {
-                    if (origOnConnectInput) {
-                        origOnConnectInput.apply(this, arguments);
+                    if (type === 1) { // 1 = LiteGraph.INPUT
+                        updateImageInputs(index, connected);
+                        setTimeout(() => updateImageInputs(), 10);
                     }
-                    setTimeout(() => updateImageInputs(), 20);
-                };
-
-                const origOnDisconnectInput = this.onDisconnectInput;
-                this.onDisconnectInput = function () {
-                    if (origOnDisconnectInput) {
-                        origOnDisconnectInput.apply(this, arguments);
-                    }
-                    setTimeout(() => updateImageInputs(), 20);
                 };
 
                 const origOnConfigure = this.onConfigure;
@@ -90,7 +86,7 @@ app.registerExtension({
                         origOnConfigure.apply(this, arguments);
                     }
                     this._configuring = false;
-                    setTimeout(() => updateImageInputs(), 100);
+                    setTimeout(() => updateImageInputs(), 50);
                 };
 
                 setTimeout(() => updateImageInputs(), 50);
